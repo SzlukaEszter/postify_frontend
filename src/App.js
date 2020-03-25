2e481f9dca79a8d5802acb7214b2e7657535f136
Eszter Szluka <szluka.eszter@gmail.com>
2020-03-25 18:27:37 +0100

    add initial plan for main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>header
+      <div>logo</div>
+      <div>menu</div>
+      <div>username</div>
+      <div >logout></div>
+    </div>
+    <div>campaign title</div>
+    <div>calendar
+      <button>prev week</button>
+      <div>This Week</div>
+      <button>next week</button>
+      <div>calendar header</div>
+      <div>day <button>add content</button></div>
+      <div>content
+        <div>media <button>upload media</button></div>
+        <div>title</div>
+        <div>lead</div>
+        <div>body</div>
+        <div>hashtag</div>
+        <div>channel</div>
+      </div>
+      <div>day <button>add content</button></div>
+      <div>day <button>add content</button></div>
+      <div>day <button>add content</button></div>
+      <div>day <button>add content</button></div>
+      <div>day <button>add content</button></div>
+      <div>day <button>add content</button></div>
+    </div>
     </div>
   );
 }
